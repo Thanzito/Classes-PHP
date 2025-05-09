ef6481e644884f9a92159cb2b1b099dc9d4056af
Thanzito <163420206+Thanzito@users.noreply.github.com>
2025-05-09 12:05:02 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once "src/Classes/Produto.php";
+require_once "src/Classes/Cliente.php";
+
+
+$prod1 =  new Produto;
+$prod1 ->titulo = "Fanta uva";
+$prod1 ->descricao = "Refrigerante 350ml";
+$prod1 ->preco = 3.50;
+
+$prod2 = new Produto;
+$prod2 ->titulo = "Chetoos";
+$prod2 ->descricao = "Salgadinho";
+$prod2 ->preco = 14.00;
+
+var_dump($prod1);
+echo "<br>";
+var_dump($prod2);
+echo "<br>";
+
+$cli = new Cliente;
+$cli ->nome = "Nathan";
+$cli ->idade = 27;
+$cli ->endereco = "Rua A, 123";
+$cli ->telefone = "(41) - 9 - XXXX-XXXX";
+$cli ->compar();
