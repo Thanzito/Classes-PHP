264fce530e9e38a0723a4b4a382b6df59a802b28
Thanzito <163420206+Thanzito@users.noreply.github.com>
2025-05-26 10:10:41 -0300

    Add files via upload

--- this.php
+++ this.php
@@ -2,3 +2,18 @@
 
 require_once "src/Classes/Cliente.php";
 
+$cli = new Cliente;
+$cli ->nome = "João da Silva";
+$cli ->idade = 40;
+$cli ->endereco = "Rua A, 123";
+$cli ->telefone = " (41) 99999-9999";
+
+$cli ->comprar();
+echo "<br>";
+
+$cli2 = new Cliente;
+$cli2 ->definirNome("Maria");
+$cli2 ->idade = 30;
+$cli2 ->endereco = "Rua B, 456";
+$cli2 ->telefone = " (41) 88888-8888";
+$cli2 ->comprar();
